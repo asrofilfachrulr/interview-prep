f16d693781551378673e3042da79a32a79969640
cucumber1420 <riidloa@gmail.com>
2022-01-25 06:41:43 +0700

    alhamdulillah

---
+++ 2d-arr-ds.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "math"
+)
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+// https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+func hourglassSum(arr [][]int32) int32 {
+    // Write your code here
+    var maxSum  int32 = math.MinInt32 // don't initialize this with 0
+    var sum int32
+    
+    for r := 0; r < 4; r++{
+        for c := 0; c < 4; c++{
+            sum += arr[r][c] + arr[r][c+1] + arr[r][c+2] + arr[r+1][c+1] + arr[r+2][c] + arr[r+2][c+1] + arr[r+2][c+2]
+        if maxSum <= sum {
+            maxSum = sum
+        }
+        sum = 0            
+        }
+    }
+    fmt.Println(maxSum)  
+    return maxSum
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    var arr [][]int32
+    for i := 0; i < 6; i++ {
+        arrRowTemp := strings.Split(strings.TrimRight(readLine(reader)," \t\r\n"), " ")
+
+        var arrRow []int32
+        for _, arrRowItem := range arrRowTemp {
+            arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+            checkError(err)
+            arrItem := int32(arrItemTemp)
+            arrRow = append(arrRow, arrItem)
+        }
+
+        if len(arrRow) != 6 {
+            panic("Bad input")
+        }
+
+        arr = append(arr, arrRow)
+    }
+
+    result := hourglassSum(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
