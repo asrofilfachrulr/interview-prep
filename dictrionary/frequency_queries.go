cbe4f21b6b00dcc6604d5e7ebf406a1620da1e31
asrofil <riidloa@gmail.com>
2022-02-02 18:19:36 +0700

    approach new method: halfway

--- dictrionary/frequency_queries.go
+++ dictrionary/frequency_queries.go
@@ -5,43 +5,82 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 )
+
+func moveToNewKey(freq map[int32]map[int32]bool, t int32, b int32, a int32) {
+	// b stands for key before and a stands before key after
+	// a == 0 if no need to move and only delete
+
+	// delete from previous group
+	delete(freq[b], t)
+	// add to new group, if willing
+	if a != 0 {
+		freq[a][t] = true
+	}
+
+	// check if current group is empty, delete
+	if len(reflect.ValueOf(freq[b]).MapKeys()) == 0 {
+		delete(freq, b)
+	}
+}
+
+func logData(d map[int32]int32, f map[int32]map[int32]bool, before bool) {
+	if before {
+		fmt.Printf("data before: %v\n", d)
+		fmt.Printf("freq before: %v\n", f)
+		return
+	}
+	fmt.Printf("data now: %v\n", d)
+	fmt.Printf("freq now: %v\n", f)
+}
 
 // Complete the freqQuery function below.
 func freqQuery(queries [][]int32) []int32 {
 	result := []int32{}
 	data := make(map[int32]int32)
+	freq := make(map[int32]map[int32]bool)
 	for _, q := range queries { // adding data
+		fmt.Printf("\ninput now: %v\n", q)
+		logData(data, freq, true)
 		if q[0] == 1 {
 			if _, f := data[q[1]]; f {
+				b := data[q[1]]
 				data[q[1]] += 1
+
+				// move to new occurence group
+				moveToNewKey(freq, q[1], b, b+1)
 			} else {
 				data[q[1]] = 1
+				if _, f := freq[1]; f {
+					freq[1][q[1]] = true
+				} else {
+					freq[1] = map[int32]bool{q[1]: true}
+				}
 			}
 		} else if q[0] == 2 { // remove data
 			if _, f := data[q[1]]; f {
+				b := data[q[1]]
 				data[q[1]] -= 1
 
 				// remove an int if it has 0 frequency
 				if data[q[1]] == 0 {
 					delete(data, q[1])
 				}
+
+				moveToNewKey(freq, q[1], b, b-1)
+
 			}
 		} else { // find data whose such given frequency (q[1])
-			isFounded := false
-			for d := range data {
-				if data[d] == q[1] {
-					result = append(result, 1)
-					isFounded = true
-					break
-				}
-			}
-			if !isFounded {
+			if _, f := freq[q[1]]; f {
+				result = append(result, 1)
+			} else {
 				result = append(result, 0)
 			}
 		}
+		logData(data, freq, false)
 	}
 	return result
 }
